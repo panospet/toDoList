5cdc838448f539abc231a55e73167f90daa56a3e
panos <panospet@gmail.com>
2016-07-10 12:02:21 +0300

    first test with selenium successfull

--- toDoList/tests.py
+++ toDoList/tests.py
@@ -25,40 +25,23 @@ class TestTodo(TestCase):
 		self.assertEquals(objectindb.title, 'testtitle')
 
 
+class MySeleniumTests(StaticLiveServerTestCase):
+    fixtures = ['user-data.json']
 
+    @classmethod
+    def setUpClass(cls):
+        super(MySeleniumTests, cls).setUpClass()
+        cls.selenium = WebDriver()
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(MySeleniumTests, cls).tearDownClass()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class MySeleniumTests(StaticLiveServerTestCase):
-#     fixtures = ['user-data.json']
-
-#     @classmethod
-#     def setUpClass(cls):
-#         super(MySeleniumTests, cls).setUpClass()
-#         cls.selenium = WebDriver()
-
-#     @classmethod
-#     def tearDownClass(cls):
-#         cls.selenium.quit()
-#         super(MySeleniumTests, cls).tearDownClass()
-
-#     def test_login(self):
-#         self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
-#         username_input = self.selenium.find_element_by_name("username")
-#         username_input.send_keys('admin')
-#         password_input = self.selenium.find_element_by_name("password")
-#         password_input.send_keys('qwe123qwe')
-#         self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
+    def test_login(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('admin')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('qwe123qwe')
+        self.selenium.find_element_by_id('login-button').click()
